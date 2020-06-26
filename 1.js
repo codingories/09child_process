da587a6f541cffe7b721a66dcbc96dd2d243c29e
朱骏杰 <ories.zhu@mochain.vip>
2020-06-26 21:45:59 +0800

    options常用几个值

--- 1.js
+++ 1.js
@@ -4,10 +4,14 @@ const {execFile} = child_process
 
 const userInput = '.'
 
-const streams = execFile('ls', ['-la', userInput]);
+const streams = execFile('ls', ['-la', userInput],{
+  cwd: '../',  // cwd表示默认在哪个路径下选择
+  env: {NODE_ENV: 'development'}, // 环境变量
+  shell: '', // 指定shell路径，一般不用,
+  maxBuffer: 1024*1024, // 设置最大缓存,最大2mb,1024*1024
+},(error, stdout)=>{
+  console.log(error) // 执行完再回调
+  console.log(stdout)
+});
 
-
-streams.stdout.on('data', (chunk)=>{
-  console.log(chunk)
-})
 
