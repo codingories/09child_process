17a69ba94493827041f5865972f9e08c087f7295
朱骏杰 <ories.zhu@mochain.vip>
2020-06-26 21:20:04 +0800

    返回一个流

--- 1.js
+++ 1.js
@@ -1,8 +1,18 @@
 const child_process = require('child_process')
 const {exec} = child_process
 
-exec('ls ../',(error, stdout,stderr)=>{
-  console.log(error)
-  console.log(stdout)
-  console.log(stderr)
+const streams = exec('ls -l ../')
+
+streams.stdout.on('data', (chunk)=>{
+  console.log('得到了数据')
+  console.log(chunk)
 })
+
+streams.stderr.on('data')
+
+
+// exec('ls ../',(error, stdout,stderr)=>{
+//   console.log(error)
+//   console.log(stdout)
+//   console.log(stderr)
+// })
