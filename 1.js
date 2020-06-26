0119bfa363cc44a7a0f1b9e8b708f8c9a2472789
朱骏杰 <ories.zhu@mochain.vip>
2020-06-26 22:28:03 +0800

    更新spawn

--- 1.js
+++ 1.js
@@ -7,8 +7,6 @@ const userInput = '.'
 const streams = spawn('ls', ['-la', userInput], {
   cwd: '../',  // cwd表示默认在哪个路径下选择
   env: {NODE_ENV: 'development'}, // 环境变量
-  shell: '', // 指定shell路径，一般不用,
-  maxBuffer: 1024 * 1024, // 设置最大缓存,最大2mb,1024*1024
 });
 
 streams.stdout.on('data',(chunk)=>{
