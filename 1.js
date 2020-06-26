be996de8806784770b2a537c68b4e6918a5ed0a4
朱骏杰 <ories.zhu@mochain.vip>
2020-06-26 21:36:34 +0800

    execFile支持流，且唯一好处不会引发注入

--- 1.js
+++ 1.js
@@ -4,21 +4,10 @@ const {execFile} = child_process
 
 const userInput = '.'
 
-execFile('ls', ['-la', userInput], (error, stdout)=>{
-  console.log(stdout)
+const streams = execFile('ls', ['-la', userInput]);
+
+
+streams.stdout.on('data', (chunk)=>{
+  console.log(chunk)
 })
 
-
-// streams.stdout.on('data', (chunk)=>{
-//   console.log('得到了数据')
-//   console.log(chunk)
-// })
-
-// streams.stderr.on('data')
-
-
-// exec('ls ../',(error, stdout,stderr)=>{
-//   console.log(error)
-//   console.log(stdout)
-//   console.log(stderr)
-// })
